84d13ca0c8e598bce514b70eef5ad507ec836648
zrivest <zrivest@gmail.com>
2013-10-30 10:22:50 -0500

    dont render layout if ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :no_layout_if_xhr
+
+
+  private
+
+  def no_layout_if_xhr
+    request.xhr? ? false : "application"
+  end
 end
